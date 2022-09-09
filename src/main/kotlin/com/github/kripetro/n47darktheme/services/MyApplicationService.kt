598c79b91d5e70bbb78bc6d02b2c7663bf5992f0
GitHub Action <action@github.com>
2022-09-09 10:49:15 +0000

    Template cleanup

--- src/main/kotlin/com/github/kripetro/n47darktheme/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/kripetro/n47darktheme/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.kripetro.n47darktheme.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.kripetro.n47darktheme.MyBundle
 
 class MyApplicationService {
 
